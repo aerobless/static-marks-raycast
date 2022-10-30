1d3642243ea86da4b93f08625aa38e8a6825be7e
Theo Winter <theo.winter@gmail.com>
2022-10-30 15:43:44 +0100

    Made extension ready for release in raycast store

--- code/static-marks/src/index.tsx
+++ code/static-marks/src/index.tsx
@@ -35,7 +35,6 @@ function SearchListItem({searchResult}: { searchResult: LinkResult }) {
     return (
         <List.Item
             title={searchResult.name}
-            //subtitle={searchResult.description}
             actions={
                 <ActionPanel>
                     <ActionPanel.Section>
